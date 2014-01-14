878c025369d555044aa5e559d16994350fbdcb4d
Michael Lawson <lawsonro3@gmail.com>
2014-01-14 14:03:36 -0700

    Added Apache 2.0 license information
    Added LICENSE and NOTICE files
    
    Added headers to all source code

--- Source/find_rpm.m
+++ Source/find_rpm.m
@@ -1,3 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright 2014 Danny Sale and Michael Lawson
+% 
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+% 
+%     http://www.apache.org/licenses/LICENSE-2.0
+% 
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function rpm = find_rpm(WTPoup,state,WTP,SIM,TURB,CAV,ENV,BLADE)
        
 % find the flow speeds for region 3 (where power is above rated power and rpm will vary to shed excess power & loads)
--- Source/guessOptimalClCd.m
+++ Source/guessOptimalClCd.m
@@ -1,3 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright 2014 Danny Sale and Michael Lawson
+% 
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+% 
+%     http://www.apache.org/licenses/LICENSE-2.0
+% 
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function OPT = guessOptimalClCd(OPT,TURB,AF)
 OPT.guess.tsr  = 4*pi/TURB.NUM_BLADES;                      % make a guess at the optimal tip speed ratio
 [OPT.guess.L_D OPT.guess.designL_D_ind] = max(AF.maxL_D);   % why are we picking the maximum l/d across all reynolds numbers instead of the Reynolds number at which the blade OPT.objectivections?
